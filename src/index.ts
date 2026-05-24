ece79b442eba7ec139d2015f72b76402f0fa8f68
mackenzie414 <fauzyaditya414@gmail.com>
2026-05-24 11:32:33 +0700

    fix port for railway

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import categoryRoute from './routes/categoryRoute.js';
 import speakerRoute from './routes/speakerRoutes.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
